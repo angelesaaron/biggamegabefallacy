75e39c0dc4b863ddd721272916c34b3927b106e4
Aaron Angeles <asa277@cornell.edu>
2024-12-09 22:04:18 -0500

    Model Refactor 1.1.8

--- streamlit-app.py
+++ streamlit-app.py
@@ -29,6 +29,7 @@ st.title("Big Game Gabe Touchdown Model?")
 st.write("Pick an NFL pass catcher and see if they're due for a Gabe Davis style Big Game")
 year, week = get_current_nfl_week()
 st.write(f"Week {week}")
+st.write(datetime.today())
 # Tabs
 tab_player, tab_best_odds, tab_performance, tab_faq = st.tabs(["Receiving TD Model", "Weekly Best Odds", 'Past Performance', 'FAQ'])
 
